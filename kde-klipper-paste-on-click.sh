22a96dee46a1ffcbc8ead3f612c9e65cb0be6e6f
Alex <alex@khv.me>
2026-04-16 15:28:20 +0300

    verbose option added

--- kde-klipper-paste-on-click.sh
+++ kde-klipper-paste-on-click.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
+
+VERBOSE=false
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -v|--verbose)
+      VERBOSE=true
+      shift # Move to the next argument
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
+
 # Monitor for clipboard changes in KDE Klipper
 dbus-monitor "sender='org.kde.klipper',interface='org.kde.klipper.klipper'" | \
 while read -r line; do
     # When a line containing the signal is detected, run your code
     if echo "$line" | grep -q "clipboardHistoryUpdated"; then
         windowname=`kdotool getactivewindow getwindowname`
+        if [ "$VERBOSE" = true ]; then
+            echo "[$(date +'%H:%M:%S')] clipboardHistoryUpdated for $windowname"
+        fi
         if [[ "$windowname" == "Clipboard Popup" || "$windowname" == "plasmashell" ]]; then
+            if [ "$VERBOSE" = true ]; then
+                echo "[$(date +'%H:%M:%S')] trigger paste command"
+            fi
             sleep 0.1 && ydotool key 42:1 110:1 110:0 42:0
         fi
     fi
